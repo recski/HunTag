afd2066dae03fec7eedd4e5459d1b615c40e74aa
Gabor Recski <gabor@cliqz.com>
2014-03-03 19:58:55 +0100

    added feature that allows to process all file in a directory while loading the models only once also made the three source files conform strictly to PEP8 (via flakes8) 	modified:   huntag.py 	modified:   tagger.py 	modified:   tools.py

--- tools.py
+++ tools.py
@@ -1,5 +1,4 @@
 #Miscellaneous tools for HunTag
-# vim: set expandtab: 
 from collections import defaultdict
 import sys
 def sentenceIterator(input):
@@ -10,28 +9,26 @@ def sentenceIterator(input):
         if not line:
             break
         if line.startswith('"""'):
-            if currSen:
-                sys.stderr.write('ERROR: comments are only allowed before a sentence\n')
-                sys.exit(-1)
-            else:
-                currComment = line.strip()
-                continue
-        if line == '\n' and currSen!=[]:
+            assert currSen == [], 'ERROR: \
+                comments are only allowed before a sentence\n'
+            currComment = line.strip()
+            continue
+        if line == '\n' and currSen != []:
             yield currSen, currComment
             currSen = []
             currComment = None
             continue
         currSen.append(line.strip().split())
-    if currSen!=[]:
+    if currSen != []:
         yield currSen, currComment
     return
 
-def writeSentence(sen, comment):
+def writeSentence(sen, out=sys.stdout, comment=None):
     if comment:
-        print comment
+        out.write(comment+'\n')
     for tok in sen:
-        print '\t'.join(tok)
-    print
+        out.write('\t'.join(tok)+'\n')
+    out.write('\n')
 
 def addTagging(sen, tags):
     taggedSen = []
@@ -39,11 +36,11 @@ def addTagging(sen, tags):
         taggedSen.append(tok+[tags[c]])
     return taggedSen
 
-def featurizeSentence(sen, features):      
-    sentenceFeats = [ [] for _ in range(len(sen)) ]
+def featurizeSentence(sen, features):
+    sentenceFeats = [[] for _ in range(len(sen))]
     for name, feature in features.items():
         for c, feats in enumerate(feature.evalSentence(sen)):
-            sentenceFeats[c]+=feats
+            sentenceFeats[c] += feats
     return sentenceFeats
 
 class BookKeeper():
@@ -52,22 +49,22 @@ class BookKeeper():
         self.featToNo = {}
         self.noToFeat = {}
         self.next = 1
-    
+
     def cutoff(self, cutoff):
         self.featCounter = dict([(feat, count)
                          for feat, count in self.featCounter.iteritems()
-                         if count>=cutoff])
+                         if count >= cutoff])
         keptPairs = [(feat, no) for feat, no in self.featToNo.iteritems()
-                     if self.featCounter.has_key(feat)]
+                     if feat in self.featCounter]
         self.featToNo = dict(keptPairs)
-        self.noToFeat = dict([(no, feat) for feat, no in keptPairs])        
+        self.noToFeat = dict([(no, feat) for feat, no in keptPairs])
 
     def getNo(self, feat):
-        self.featCounter[feat]+=1
-        if not self.featToNo.has_key(feat):
+        self.featCounter[feat] += 1
+        if not feat in self.featToNo:
             self.featToNo[feat] = self.next
             self.noToFeat[self.next] = feat
-            self.next+=1
+            self.next += 1
         return self.featToNo[feat]
 
     def saveToFile(self, fileName='featureNumbers.txt'):
